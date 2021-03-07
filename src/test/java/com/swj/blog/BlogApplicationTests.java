7a0aae17172fe8db815b118e411f0103dda0e784
SWJ8529 <852922827@qq.com>
2021-03-07 20:53:01 +0800

    前端组件配置，后端框架构建

---
+++ src/test/java/com/swj/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swj.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
