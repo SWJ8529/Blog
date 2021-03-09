aa9ca3b5f5a3c837ce0ebf84e6f8780c10b6574a
SWJ8529 <852922827@qq.com>
2021-03-09 23:17:12 +0800

    添加日志处理，和页面引用

--- src/main/java/com/swj/web/IndexController.java
+++ src/main/java/com/swj/web/IndexController.java
@@ -3,16 +3,18 @@ package com.swj.web;
 import com.swj.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
-    public String index(){
-        System.out.println("index");
-        String blog = null;
-        if(blog == null){
-            throw new NotFoundException("博客不存在");
-        }
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name){
+//        System.out.println("index");
+//        String blog = null;
+//        if(blog == null){
+//            throw new NotFoundException("博客不存在");
+//        }
+        System.out.println("-------------index--------------");
         return "index";
     }
 }
